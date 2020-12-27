f0ebdca589ee20393a7fcfc28f8797549f8ca5c7
Christian Weiske <cweiske@cweiske.de>
2020-12-27 12:42:38 +0100

    Clean game folders

--- bin/clean-generated.sh
+++ bin/clean-generated.sh
@@ -5,6 +5,7 @@ rm www/api/v1/details-data/*
 rm -r www/api/v1/developers/*
 rm www/api/v1/discover-data/*
 rm www/api/v1/games/*/*
+rmdir www/api/v1/games/*/
 rm www/api/v1/search-data/*
 rm www/discover/*
 rm www/game/*
