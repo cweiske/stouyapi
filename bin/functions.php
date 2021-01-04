899f34b00c0978dcb3639b04bba532f5185a0fc6
Christian Weiske <cweiske@cweiske.de>
2021-01-04 07:11:39 +0100

    fix tutorials game details link

--- bin/functions.php
+++ bin/functions.php
@@ -5,6 +5,10 @@
 
 function categoryPath($title)
 {
+    if ($title == 'Tutorials') {
+        //OUYAs fetch "Make" from "discover/tutorials"
+        $title = strtolower($title);
+    }
     return str_replace(['/', '\\', ' ', '+', '?', '!'], '_', $title);
 }
 ?>
